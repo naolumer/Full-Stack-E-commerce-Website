64be0e786b150b6fd9f4c0c392a5672497cf36b5
naolumer <naolumer3@gmail.com>
2025-01-31 23:01:24 +0300

    Hero section left-side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import About from './pages/About'
 import Login from './pages/Login'
 import Navbar from './components/Navbar'
 
+
 function App() {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import Hero from '../components/Hero'
 
 function Home() {
   return (
-    <div>Home</div>
+    <Hero/>
   )
 }
 
