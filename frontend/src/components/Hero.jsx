59f6e2f31dbacbfdc6fdb20edbc389919c4a9a1d
naolumer <naolumer3@gmail.com>
2025-02-01 09:47:13 +0300

    right-side hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import {assets} from '../assets/assets'
 function Hero() {
   return (
     <div className='flex flex-col sm:flex-row border border-gray-400'>
@@ -12,7 +11,7 @@ function Hero() {
                     <p className='w-8 md:w-11 h-[2px] bg-[#414141]'/>
                     <p className='font-medium text-sm md:text-base'>OUR BESTSELLLERS</p>
                 </div>
-                <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+                <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
                 <div className='flex items-center gap-2'>
                     <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
                     <p className='w-8 md:w-11 h-[2px] bg-[#414141]'/>
@@ -21,6 +20,7 @@ function Hero() {
         </div>
         
         {/* {Right-Side Hero} */}
+        <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
         
     </div>
   )
