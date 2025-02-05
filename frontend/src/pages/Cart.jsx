9f08a5c763b186255e7c68b07fc0264647f0d6ba
naolumer <naolumer3@gmail.com>
2025-02-05 15:07:27 +0300

    display price including (total,subtotal,shipping_fee)

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,9 +2,10 @@ import React, { useState,useContext, useEffect } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 function Cart() {
-  const {products,currency,cartItems,updateQuantity} = useContext(ShopContext);
+  const {products,currency,cartItems,updateQuantity,navigate} = useContext(ShopContext);
 
   const [cartData,setCartData] = useState([])
 
@@ -52,6 +53,15 @@ function Cart() {
             )
           })
         }
+      </div>
+
+      <div className='flex justify-end my-20'>
+          <div className='w-full sm:w-[450px]'>
+            <CartTotal/>
+            <div className='w-full text-end'>
+              <button onClick={()=>navigate("/place-order")} type='button' className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
+            </div>
+          </div>
       </div>
     </div>
   )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,6 @@ function Home() {
     <BestSeller/>
     <OurPolicy/>
     <Newsletter/>
-    <Footer/>
     </>
     
   )
