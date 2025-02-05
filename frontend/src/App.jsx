54ef35d338f340f0d0ba8e1eb788053203ad952b
naolumer <naolumer3@gmail.com>
2025-02-05 11:50:54 +0300

    display number of added products on the cart icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,11 +11,14 @@ import Login from './pages/Login'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from "./components/SearchBar"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
       <Navbar/>
       <SearchBar/>
       <Routes>
