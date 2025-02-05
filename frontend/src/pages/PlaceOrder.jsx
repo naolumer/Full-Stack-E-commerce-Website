7d015f376b9be69d689b21e8a899995542f37820
naolumer <naolumer3@gmail.com>
2025-02-05 17:05:19 +0300

    Delivery page left section

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Title from '../components/Title'
 
 function PlaceOrder() {
   return (
-    <div>PlaceOrder</div>
+    <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
+      {/* {Left Side} */}
+      <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
+        <div className='text-xl sm:text-2xl my-3'>
+          <Title text1={"DELIVERY"} text2={"INFORMATION"}/>
+        </div>
+        <div className='flex gap-3'>
+          <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="text" placeholder='First name' />
+          <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Last name' />
+        </div>
+        <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="text" placeholder='email' />
+        <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Street' />
+        <div className='flex gap-3'>
+          <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="text" placeholder='City' />
+          <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="text" placeholder='State' />
+        </div>
+        <div className='flex gap-3'>
+          <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Zip Code' />
+          <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Country' />
+        </div>
+        <input className='border border-gray-400 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Phone' />
+      </div>
+
+    </div>
   )
 }
 
