27be00197e772eb92b12b0aaf144e3fd18c4c81c
naolumer <naolumer3@gmail.com>
2025-02-05 10:49:05 +0300

    related products section

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProduct from '../components/RelatedProduct';
 
 function Product() {
   const {productId} = useParams();
@@ -88,6 +89,7 @@ function Product() {
     </div>
 
     {/* {Display Related Produts} */}
+    <RelatedProduct category={productData.category} subCategory={productData.subCategory}/>
 
     </div>
   ): <div className='opacity-0'></div>
