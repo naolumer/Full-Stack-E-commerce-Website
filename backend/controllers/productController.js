880479999723044125bf060b3e3945659ff68fce
naolumer <naolumer3@gmail.com>
2025-02-08 17:37:42 +0300

    single product route handler function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -69,7 +69,16 @@ const removeProduct = async(req,res)=>{
 
 // function for single product info
 const singleProduct = async(req,res)=>{
-    
+   try{
+    const {productId} = req.body;
+    const product = await productModel.findById(productId)
+    res.json({
+        success: true, product
+    })
+
+   } catch(error){
+    res.json({success:false,msg: error.message})
+   } 
 }
 
 export {listProducts, addProduct,removeProduct, singleProduct}
