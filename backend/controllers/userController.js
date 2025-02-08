a72c514e1e3c4e24a28e214ee9fabc044786625f
naolumer <naolumer3@gmail.com>
2025-02-08 18:02:05 +0300

    admin authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,7 +74,18 @@ const registerUser = async (req,res)=>{
 // Route for admin  login
 
 const adminLogin = async (req,res)=>{
+    try {
+        const {email,password}= req.body;
 
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password,process.env.JWT_SECRET);
+            res.json({success:true,token})  
+        } else {
+            res.json({success:false, message:"Invalid credentials"})
+        }      
+    } catch {
+
+    }
 }
 
 export {loginUser,registerUser, adminLogin}
