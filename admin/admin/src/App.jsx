c83e367241c3df9eb972435e8b63be26310672e5
naolumer <naolumer3@gmail.com>
2025-02-08 22:04:37 +0300

    admin sidebar and routes

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import {Routes,Route} from 'react-router-dom'
+import List from './pages/List'
+import Add from './pages/Add'
+import Orders from './pages/Orders'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
+      <Navbar/>
+      <hr />
+      <div className='flex w-full'>
+        <Sidebar/>
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+          <Routes>
+            <Route path='/add' element={<Add/>}/>
+            <Route path='/list' element={<List/>}/>
+            <Route path='/orders' element={<Orders/>}/>
+     
+          </Routes>
+        </div>
+      </div>
+    </>
+    </div>
+    
   )
 }
 
---
+++ admin/admin/src/pages/Orders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Orders() {
+  return (
+    <div>Orderrs</div>
+  )
+}
+
+export default Orders
