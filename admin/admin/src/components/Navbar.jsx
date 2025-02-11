fe39786c82b0b09bbfee80d523b744436184a381
naolumer <naolumer3@gmail.com>
2025-02-11 22:06:01 +0300

    login & logout functionality for admin dashboard

--- admin/admin/src/components/Navbar.jsx
+++ admin/admin/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {assets} from "../assets/assets"
 
-function Navbar() {
+function Navbar({setToken}) {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
         <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-        <button type="button" className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full'>Logout</button>
+        <button onClick={()=>setToken("")} type="button" className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full'>Logout</button>
 
     </div>
   )
